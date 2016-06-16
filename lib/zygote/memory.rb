2b7bfa7946a9332b658ac6efc454499439faa7d3
Dale Hamel <dale.hamel@shopify.com>
2016-06-16 11:28:49 -0400

    Various style fixes

--- lib/zygote/memory.rb
+++ lib/zygote/memory.rb
@@ -3,24 +3,27 @@ require 'fileutils'
 
 require 'supermodel'
 
-# A simple means of persistence
-# This can easily be swapped out for redis, but file-based is simpler and good enough for now
-# https://github.com/maccman/supermodel/blob/master/README
-module Memory
-  extend self
-  DATABASE_PATH = (ENV['DATABASE_PATH'] || File.expand_path('../data/memory.db', $PROGRAM_NAME)).freeze
-  SuperModel::Marshal.path = DATABASE_PATH
+# Zygote container
+module Zygote
+  # A simple means of persistence
+  # This can easily be swapped out for redis, but file-based is simpler and good enough for now
+  # https://github.com/maccman/supermodel/blob/master/README
+  module Memory
+    extend self
+    DATABASE_PATH = (ENV['DATABASE_PATH'] || File.expand_path('../data/memory.db', $PROGRAM_NAME)).freeze
+    SuperModel::Marshal.path = DATABASE_PATH
 
-  def save
-    FileUtils.mkdir_p(File.dirname(DATABASE_PATH)) # FIXME: - don't make if it already exists
-    SuperModel::Marshal.dump
+    def save
+      FileUtils.mkdir_p(File.dirname(DATABASE_PATH)) # FIXME: - don't make if it already exists
+      SuperModel::Marshal.dump
+    end
+
+    def load
+      SuperModel::Marshal.load
+    end
   end
 
-  def load
-    SuperModel::Marshal.load
+  at_exit do
+    Memory.save
   end
-end
-
-at_exit do
-  Memory.save
 end
